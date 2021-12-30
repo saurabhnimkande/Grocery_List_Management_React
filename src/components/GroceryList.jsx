5136f53245688aa13b0fa519e8acdf3f99e8e521
SaurabhN16 <91530878+saurabhnimkande@users.noreply.github.com>
2021-12-30 09:33:48 +0530

    Update GroceryList.jsx

--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -1,9 +1,13 @@
 import "./GroceryList.css";
-export const GroceryList = ({title,id,deleteitem,i}) => {
-   
-    return (
-        <>
-        <h2>{i+1}. {title} <button onClick={() => deleteitem(id)} className="deletebutton">Delete</button></h2>
-        </>
-    )
-}
+export const GroceryList = ({ title, id, deleteitem, i }) => {
+  return (
+    <>
+      <h2>
+        {i + 1}. {title}{" "}
+        <button onClick={() => deleteitem(id)} className="deletebutton">
+          Delete
+        </button>
+      </h2>
+    </>
+  );
+};
